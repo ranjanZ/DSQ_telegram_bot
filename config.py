57e950c8ac3598dcc2fa33e5ccc0109e4b6c4b23
ranjanZ <ranjan.rbc@gmail.com>
2026-06-10 00:25:12 +0530

    congid without key added

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
-# Telegram token
 
-
-# GitHub configuration for licence saving
 GITHUB_REPO = "ranjanZ/Dalal_Street_Quants"
-FILE_PATH = "V1_user_id.json"
+
+#V1 file path and API URL
+FILE_PATH_V1 = "V1_user_id.json"
 BRANCH = "main"
-GITHUB_API_URL = f"https://api.github.com/repos/{GITHUB_REPO}/contents/{FILE_PATH}"
+GITHUB_API_URL_V1 = f"https://api.github.com/repos/{GITHUB_REPO}/contents/{FILE_PATH_V1}"
+
+
+
+#V2 file path and API URL
+FILE_PATH_V2 = "V2_user_id.json"
+BRANCH = "main"
+GITHUB_API_URL_V2 = f"https://api.github.com/repos/{GITHUB_REPO}/contents/{FILE_PATH_V2}"
